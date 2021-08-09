d03062bd45a941ae78e1b6ba3212198c0fb8d897
hossam <hossamxp33>
2021-08-09 15:13:10 +0200

    Recommends Fragment (layout blur for Vip)

--- app/src/main/java/com/example/tadawol/app/presentation/recommendation_fragment/RecommendationFragment.kt
+++ app/src/main/java/com/example/tadawol/app/presentation/recommendation_fragment/RecommendationFragment.kt
@@ -4,17 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tadawol.R
 import com.example.tadawol.app.presentation.viewmodel.MainViewModel
 import com.example.tadawol.databinding.RecommendationsFragmentBinding
 import kotlinx.android.synthetic.main.recommendations_fragment.*
-import kotlinx.android.synthetic.main.recommendations_items.*
 
 
 class RecommendationFragment : Fragment(){
